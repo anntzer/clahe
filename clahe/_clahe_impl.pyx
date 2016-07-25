2c1c7a478a06c9afaea6422b8990e21b3e30ac03
Antony Lee <anntzer.lee@gmail.com>
2016-07-24 23:49:27 -0700

    Uniquify values.

--- clahe/_clahe_impl.pyx
+++ clahe/_clahe_impl.pyx
@@ -17,20 +17,24 @@ ctypedef fused dtype:
     np.npy_int8
     np.npy_uint16
     np.npy_int16
+    np.npy_uint32
+    np.npy_int32
 
 
-def clahe(np.ndarray[dtype, ndim=2] img_orig,
+def clahe(dtype[:, :] img_orig,
           size_t kx,
           size_t ky,
           double clip_limit,
           bint multiplicative=False):
     cdef:
-        dtype[:, ::1] img = np.require(img_orig, requirements="C") - img_orig.min()
+        dtype[::1] orig_vals = np.unique(img_orig)
+        size_t[:, ::1] img = np.searchsorted(orig_vals, img_orig).view(np.uintp)
         size_t nx = img.shape[0], ny = img.shape[1], x0, y0, \
             x, y, hkx = kx // 2, hky = ky // 2, kxy = kx * ky, \
-            val, viter, vmax = np.max(img), \
+            val, viter, \
+            nvals = orig_vals.shape[0], max_val_p1 = orig_vals[nvals - 1], \
             count_clip = <size_t>(kx * ky * clip_limit), clip_sum
-        size_t[::1] hist = np.zeros(vmax + 1, np.uintp)
+        size_t[::1] hist = np.zeros(nvals, np.uintp)
         double clip_psum
         double[:, ::1] out = np.zeros_like(img, np.double)
     for x0 in range(nx - kx):
@@ -45,14 +49,14 @@ def clahe(np.ndarray[dtype, ndim=2] img_orig,
             for viter in range(val):
                 clip_sum += min(hist[viter], count_clip)
             clip_psum = clip_sum + min(hist[val] / 2., count_clip)
-            for viter in range(val, vmax):
+            for viter in range(val, nvals):
                 clip_sum += min(hist[viter], count_clip)
             out[x0 + hkx, y0 + hky] = (
                 # Multiplicative redistribution.
                 clip_psum / clip_sum
                 if multiplicative else
                 # Additive redistribution.
-                (clip_psum + (kxy - clip_sum) * (<double>val / vmax)) / kxy)
+                (clip_psum + (kxy - clip_sum) * (orig_vals[val] + .5) / max_val_p1) / kxy)
             # Update histogram.
             for x in range(kx):
                 hist[img[x0 + x, y0]] -= 1
