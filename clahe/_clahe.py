180368bc7551d76fbfe95f480883ffeb381ad968
Antony Lee <anntzer.lee@gmail.com>
2016-08-31 00:30:11 -0700

    Try to optimize the pure python version.

--- clahe/_clahe.py
+++ clahe/_clahe.py
@@ -4,21 +4,28 @@ from . import _clahe_impl
 
 from utils import timed
 @timed
-def clahe(img, kshape, clip_limit):
+def clahe(img, win_shape, clip_limit):
     """Contrast-limited adaptive histogram equalization.
     """
-    kx, ky = kshape
-    img = np.pad(img, ((kx, kx), (ky, ky)), "reflect")
-    res = _clahe_impl.clahe(img, kx, ky, clip_limit)
-    # res = _clahe_nd(img, (kx, ky), clip_limit)
-    return res[kx:-kx, ky:-ky]
+    # Bring the largest dimension to the front to optimize the in-loop
+    # histogram updates.
+    largest_dim = np.argmax(win_shape)
+    img = np.swapaxes(img, 0, largest_dim)
+    win_shape = list(win_shape)
+    win_shape[0], win_shape[largest_dim] = win_shape[largest_dim], win_shape[0]
+    img = np.pad(img, [(sz, sz) for sz in win_shape], "reflect")
+    # res = _clahe_impl.clahe(img, *win_shape, clip_limit)
+    res = _clahe_nd(img, win_shape, clip_limit)
+    return np.swapaxes(res[tuple(slice(sz, -sz) for sz in win_shape)],
+                       0, largest_dim)
 
 
 def _clahe_nd(img, win_shape, clip_limit, multiplicative=False):
     orig_vals = np.unique(img)
     img_ord = np.searchsorted(orig_vals, img)
     nvals = len(orig_vals)
-    hist = np.empty(nvals, int)
+    bincount = lambda t: np.bincount(t.flat, minlength=nvals)
+    buf = np.zeros(nvals + 1)
     out = np.zeros_like(img, float)
 
     win_shape = np.asarray(win_shape)
@@ -26,27 +33,28 @@ def _clahe_nd(img, win_shape, clip_limit, multiplicative=False):
 
     count_clip = clip_limit * win_size / nvals
 
-    for nndi in np.ndindex(*(img.shape - win_shape)[:-1]):
+    # Update on the *first* dimension in the inner loop in order to get a
+    # contiguous array in the 2D case.
+    for nndi in np.ndindex(*(img.shape - win_shape)[1:]):
         img_slice = (
-            img_ord[tuple([slice(i, i + w)
-                           for i, w in zip(nndi, win_shape[:-1])])])
-        hist[:] = 0
-        np.add.at(hist, img_slice[..., :win_shape[-1]], 1)
-        for lasti in range(img.shape[-1] - win_shape[-1]):
-            ndi = tuple((nndi + (lasti,)) + win_shape // 2)
+            img_ord[tuple([slice(None)]
+                          + [slice(i, i + w) for i, w in zip(nndi, win_shape[1:])])])
+        hist = bincount(img_slice[:win_shape[0]])
+        for idx in range(img.shape[0] - win_shape[0]):
+            ndi = tuple(((idx,) + nndi) + win_shape // 2)
             # Limit contrast.
-            clip_hist = np.minimum(hist, count_clip)
+            np.minimum(hist, count_clip, buf[1:])
             val = img_ord[ndi]
-            clip_csum = np.cumsum(clip_hist)
-            clip_sum = clip_csum[-1]
-            clip_psum = clip_csum[val] - clip_hist[val] / 2
+            np.cumsum(buf[1:], out=buf[1:])
+            clip_sum = buf[-1]
+            clip_psum = (buf[val] + buf[val + 1]) / 2
             out[ndi] = (
                 clip_psum / clip_sum
                 if multiplicative else
                 (clip_psum
                  + (win_size - clip_sum) * (orig_vals[val] + .5) / orig_vals[-1])
                 / win_size)
-            np.add.at(hist, img_slice[..., lasti], -1)
-            np.add.at(hist, img_slice[..., lasti + win_shape[-1]], 1)
+            hist -= bincount(img_slice[idx])
+            hist += bincount(img_slice[idx + win_shape[0]])
 
     return out
