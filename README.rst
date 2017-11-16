968aed45dc08d33522c916afed6dc4436237019e
Antony Lee <anntzer.lee@gmail.com>
2017-11-15 17:29:20 -0800

    Repackage.

--- README.rst
+++ README.rst
@@ -1,4 +1,4 @@
 (Exact) contrast-limited adaptive histogram equalization
 ========================================================
 
-Install with `pip install .` and test with `pytest`.  Python3 only.
+Install with ``pip install .`` and test with ``pytest``.  Python3 only.
--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import find_packages, setup
 from Cython.Build import cythonize
 
 
-if __name__ == "__main__":
-    setup(
-        name="clahe",
-        ext_modules=cythonize("**/*.pyx"),
-        packages=find_packages(include=["clahe", "clahe.*"]),
-    )
+setup(
+    name="clahe",
+    ext_modules=cythonize("**/*.pyx"),
+    packages=find_packages("lib"),
+    package_dir={"": "lib"}
+)
