ad12e8f7641eed4b6d2e478b6b3a0de515cbe704
Antony Lee <anntzer.lee@gmail.com>
2021-11-16 16:14:59 +0100

    Style, support scalar win_shape.

--- lib/clahe/_clahe.py
+++ lib/clahe/_clahe.py
@@ -11,9 +11,10 @@ def clahe(img, win_shape, clip_limit, *, _fast=True):
     ----------
     img : int array
         Input image.
-    win_shape : int sequence
+    win_shape : int sequence or int
         Shape of the contextual region.  Its length must match the
-        dimensionality of *img*.
+        dimensionality of *img*, or it can be a single int which is used as
+        size over each axis.
     clip_limit : float
         Clipping limit.
     _fast : bool, default: True
@@ -23,11 +24,13 @@ def clahe(img, win_shape, clip_limit, *, _fast=True):
     """
     # Bring the largest dimension to the front to optimize the in-loop
     # histogram updates.
+    win_shape = np.broadcast_to(win_shape, img.ndim)
     largest_dim = np.argmax(win_shape)
     img = np.swapaxes(img, 0, largest_dim)
     win_shape = list(win_shape)
     win_shape[0], win_shape[largest_dim] = win_shape[largest_dim], win_shape[0]
-    img = np.pad(img, [((sz - 1) // 2, sz // 2) for sz in win_shape], "reflect")
+    img = np.pad(
+        img, [((sz - 1) // 2, sz // 2) for sz in win_shape], "reflect")
     if _fast:
         if img.ndim == 2:
             res = _clahe_impl.clahe(
@@ -60,9 +63,9 @@ def _clahe_nd(img, win_shape, clip_limit, multiplicative=False):
     # Update on the *first* dimension in the inner loop in order to get a
     # contiguous array in the 2D case.
     for nndi in np.ndindex(*np.array(img.shape - win_shape)[1:] + 1):
-        img_slice = (
-            img_ord[tuple([slice(None)]
-                          + [slice(i, i + w) for i, w in zip(nndi, win_shape[1:])])])
+        img_slice = img_ord[tuple(
+            [slice(None)]
+            + [slice(i, i + w) for i, w in zip(nndi, win_shape[1:])])]
         hist = bincount(img_slice[:win_shape[0] - 1])
         for idx in range(img.shape[0] - win_shape[0] + 1):
             hist += bincount(img_slice[idx + win_shape[0] - 1])
@@ -77,7 +80,8 @@ def _clahe_nd(img, win_shape, clip_limit, multiplicative=False):
                 clip_psum / clip_sum
                 if multiplicative else
                 (clip_psum
-                 + (win_size - clip_sum) * (orig_vals[val] + .5) / (orig_vals[-1] + 1))
+                 + ((win_size - clip_sum)
+                    * (orig_vals[val] + .5) / (orig_vals[-1] + 1)))
                 / win_size)
             hist -= bincount(img_slice[idx])
 
--- test_clahe.py
+++ test_clahe.py
@@ -10,10 +10,10 @@ class TestClahe(TestCase):
         for win_size, img_size in [
                 ((2, 2), (4, 4)),
                 ((3, 3), (4, 4)),
-                ((4, 4), (4, 4)),
+                (4, (4, 4)),
                 ((1, 3, 3), (3, 3, 3)),
                 ((2, 3, 3), (3, 3, 3)),
-                ((3, 3, 3), (4, 4, 4)),
+                (3, (4, 4, 4)),
         ]:
             np.random.seed(0)
             img = np.random.randint(0, 256, img_size, np.uint8)
