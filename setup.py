9f54bc6dd7f349ce4f5451144c54892852ba4b63
Antony Lee <anntzer.lee@gmail.com>
2023-06-16 10:12:26 +0200

    Move to pyproject.toml.

--- setup.py
+++ setup.py
@@ -1,38 +1,10 @@
-from setupext import find_namespace_packages, setup
-
-
-@setup.add_extensions
-def make_extension():
-    from pybind11.setup_helpers import Pybind11Extension
-    yield Pybind11Extension(
-        "clahe._clahe_impl", ["src/_clahe_impl.cpp"], cxx_std=11)
+from pybind11.setup_helpers import Pybind11Extension
+from setuptools import setup
 
 
 setup(
-    name="clahe",
-    description="Exact contrast-limited adaptive histogram equalization",
-    long_description=open("README.rst", encoding="utf-8").read(),
-    author="Antony Lee",
-    url="https://github.com/anntzer/clahe",
-    license="zlib",
-    classifiers=[
-        "Development Status :: 4 - Beta",
-        "License :: OSI Approved :: zlib/libpng License",
-        "Programming Language :: Python :: 3",
+    ext_modules=[
+        Pybind11Extension(
+            "clahe._clahe_impl", ["ext/_clahe_impl.cpp"], cxx_std=11),
     ],
-    packages=find_namespace_packages("lib"),
-    package_dir={"": "lib"},
-    python_requires=">=3",
-    setup_requires=[
-        "pybind11>=2.6",
-        "setuptools_scm>=3.3",  # fallback_version support.
-    ],
-    use_scm_version={
-        "version_scheme": "post-release",
-        "local_scheme": "node-and-date",
-        "fallback_version": "0+unknown",
-    },
-    install_requires=[
-        "numpy",
-    ]
 )
